630e61889c0cbd517d62e05ec6711760d959ecbe
MarceloIlegra <marcelo.serpa@ilegr.com>
2015-08-16 10:39:46 -0300

    Combine Css Files

--- gulp-project-poc/gulpfile.js
+++ gulp-project-poc/gulpfile.js
@@ -1,8 +1,15 @@
 var gulp = require('gulp');
 var stylus = require('gulp-stylus')
+var concat = require('gulp-concat');
 
 gulp.task('stylus', function() {
   gulp.src('src/styls/**/*.styl')
       .pipe(stylus())
       .pipe(gulp.dest('./assets/css/'))
 });
+
+gulp.task('combine', function(){
+  return gulp.src('./assets/css/**/*.css')
+      .pipe(concat('all.css'))
+      .pipe(gulp.dest('./compressed/'));
+})
